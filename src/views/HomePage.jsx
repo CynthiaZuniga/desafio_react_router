461c19de4ae7ea69b422fe0c8221b4930b6ecb8f
CynthiaZuniga <cynthia.zuniga.a@gmail.com>
2024-01-27 22:26:53 -0300

    main

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+const HomePage = () => {
+  return (
+    <Container className="text-center">
+      <h1 className="pt-5">
+        Bienvenido a <span className="fw-bold">Happy Cake 🍰</span>
+      </h1>
+      <h6> El lugar de los pasteles felices </h6>
+      <span className="emoticon_edition">🎂</span>
+    </Container>
+  );
+};
+export default HomePage;
